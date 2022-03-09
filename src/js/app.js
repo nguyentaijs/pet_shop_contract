3f8829380dd78fb2755638d408b78b2c129e5aa2
TaiNT <ca_admin@kt.com>
2022-03-09 18:17:31 +0700

    Test and add frontend codes

--- src/js/app.js
+++ src/js/app.js
@@ -24,17 +24,34 @@ App = {
   },
 
   initWeb3: async function() {
-    /*
-     * Replace me...
-     */
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try {
+        // request account access
+        await window.ethereum.request({method: "eth_requestAccounts"});
+      } catch (error) {
+        console.log("User denied account access");
+      }
+    } else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+    } else {
+      App.web3Provider = new Web3.providers.HttpProvider("http://localhost:7545");
+    }
+
+    web3 = new Web3(App.web3Provider);
 
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    $.getJSON('Adoption.json', function(data) {
+      var AdoptionArtifact = data;
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+
+      App.contracts.Adoption.setProvider(App.web3Provider);
+      return App.markAdopted();
+
+    });
 
     return App.bindEvents();
   },
@@ -44,9 +61,21 @@ App = {
   },
 
   markAdopted: function() {
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+
+  App.contracts.Adoption.deployed().then(function(instance) {
+    adoptionInstance = instance;
+
+    return adoptionInstance.getAdopters.call();
+  }).then(function(adopters) {
+    for (i = 0; i < adopters.length; i++) {
+      if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
+        $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+      }
+    }
+  }).catch(function(err) {
+    console.log(err.message);
+  });
   },
 
   handleAdopt: function(event) {
@@ -54,9 +83,26 @@ App = {
 
     var petId = parseInt($(event.target).data('id'));
 
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+    
+      var account = accounts[0];
+    
+      App.contracts.Adoption.deployed().then(function(instance) {
+        adoptionInstance = instance;
+    
+        // Execute adopt as a transaction by sending account
+        return adoptionInstance.adopt(petId, {from: account});
+      }).then(function(result) {
+        return App.markAdopted();
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
   }
 
 };
